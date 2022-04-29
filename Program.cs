b87b5c253c914d3639ba0e858f8e4610eef58cc2
JOSE FELIPE DA SILVA SANTOS <josefelipesilvasantos874@gmail.com>
2022-04-29 20:38:25 -0300

    finalizado

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿int numeroDigitado;
+Console.Write("Digite um número");
+numeroDigitado = Convert.ToInt32(Console.ReadLine());
+
+bool numeroEhPar = (numeroDigitado % 2) == 0;
+
+if(numeroEhPar)
+{   
+    Console.ForegroundColor =ConsoleColor.Blue;
+    Console.WriteLine($"{numeroDigitado} é par"!);
+}
+else
+{   
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine ($"{numeroDigitado} é ímpar");
+}
+
+Console.ResetColor();
+
